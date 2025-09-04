fa11906ad9aa3418120c109d136bf9205632cc96
vbth7777 <vbth7777@gmail.com>
2025-09-04 20:34:46 +0700

    04/09/2025

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -9,6 +7,13 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "typescript",
+      "css",
+      "html",
+      "tsx",
+      "javascript",
+      "jsx",
+      "python",
       -- add more arguments for adding more treesitter parsers
     },
   },
